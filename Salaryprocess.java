d7cbf86ac9704fab43c3e1c4284d967e7096b3df
MALKKHAN ALI SYED ISMAIL S <130300669+syedisma03@users.noreply.github.com>
2023-07-04 12:39:02 +0530

    Salaryprocess.java

--- Salaryprocess.java
+++ Salaryprocess.java
@@ -2,6 +2,7 @@ package finals;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -16,17 +17,43 @@ public class Salaryprocess {
 	        Connection con2=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
 	        ResultSet rs=st.executeQuery("Select * from empl");
 	        Statement st2=con.createStatement();
+	    	Connection conn1=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+    		Statement stmt1=conn1.createStatement();
+    		String sql = "DELETE FROM salary";
+    		stmt1.executeUpdate(sql);
 	        while(rs.next())
 	        {
-	        	String name=rs.getString(1);
+	        	String names=rs.getString(7);
 	        	int sal=rs.getInt(3);
 	        	
 	        
-	        	int ava=rs.getInt(7);
+	        	int ava=rs.getInt(1);
+	        	
+	        
 	        	
 	        	if(ava==1)
 	        	{
-	        		System.out.println(name+" "+sal);
+	        		System.out.println(names+" "+sal);
+	        		try
+	        		{
+	        			
+	        		Class.forName("com.mysql.cj.jdbc.Driver");
+	        		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+	        		Statement stmt=conn.createStatement();
+	        	
+                    
+	        		stmt.executeUpdate("INSERT INTO salary(salary,name)VALUES("+sal+",'"+names+"')");
+	        		System.out.println("SUCCEED");
+	        		System.out.println("\n");
+	        		
+	        		
+	        		
+	        		}
+	        		catch(Exception ex)
+	        		{
+	        			System.out.println("ErRoR");
+	        		}
+	        		
 	        	}
 	        	
 	        }
